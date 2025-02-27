83515a693c2eaab22014e4dde6b127fd288d94ec
Zach Dodson <zdodson21@gmail.com>
2025-02-27 11:51:02 -0500

    applicable files can be found and audit called, needs testing

--- src/lib/programs/audit.js
+++ src/lib/programs/audit.js
@@ -1,3 +1,4 @@
+import { readdirSync, readFileSync, statSync } from "node:fs";
 import path from "node:path";
 import * as properties from "../css-properties.js";
 import * as styles from "../css-styles.js";
@@ -7,38 +8,31 @@ import * as ddd from "../ddd-styles.js";
  * @description Runs the audit command, to be called when `hax audit` command is run
  */
 export async function auditCommandDetected() {
-  let dddignore = dddignoreCollector();
+  const PROJECT_ROOT = process.cwd();
+  let dddignore = dddignoreInterpreter(PROJECT_ROOT);
+  
   dddignore.forEach(item => {
     console.table(item)
-  })
-  let auditList = createAuditList();
-}
+  });
 
-/**
- * @description Recursively finds and reads information from .dddignore files
- * @returns Array of file names from .dddignore(s) as strings
- */
-function dddignoreCollector() {
-  const PROJECT_ROOT = process.cwd();
-  return dddignoreInterpreter(PROJECT_ROOT);
+  auditNavigator(PROJECT_ROOT, dddignore);
 }
 
 /**
  * @description Gets items from dddignore with a hierarchy (.dddignore affects folders below it, never above it)
- * @returns list of files, directories, and file extensions to ignore, interpretted from .dddignore files
+ * @returns Array of objects detailing what directories, files, and file extensions to ignore
  */
 function dddignoreInterpreter(root) {
-  const fs = require('node:fs');
   let list = [];
 
-  fs.readdirSync(root).forEach(item => {
+  readdirSync(root).forEach(item => {
     const FULL_PATH = path.join(root, item);
 
-    if (item !== "node_modules"  && item !== ".git" && fs.statSync(FULL_PATH).isDirectory()) { // Directory
+    if (item !== "node_modules"  && item !== ".git" && item !== "dist" && statSync(FULL_PATH).isDirectory()) { // Directory
       list = list.concat(dddignoreInterpreter(FULL_PATH));
     }
     else if (item === ".dddignore") { // File 
-      let lines = fs.readFileSync(FULL_PATH, 'utf-8').split('\n').filter(Boolean);
+      let lines = readFileSync(FULL_PATH, 'utf-8').split('\n').filter(Boolean);
       lines.forEach(line => {
         let trimmed = line.trim();
         
@@ -49,14 +43,14 @@ function dddignoreInterpreter(root) {
             trimmed = trimmed.substring(1);
             type = "directory";
           }
-          else if  (trimmed.startsWith('*')) {
+          else if (trimmed.startsWith('*')) {
             trimmed = trimmed.substring(1);
             type = "extension";
           }
           
           const OBJECT = {
             "highest_path": root,
-            "ignore": trimmed,
+            "name": trimmed,
             "type": type
           };
 
@@ -74,17 +68,44 @@ function dddignoreInterpreter(root) {
 }
 
 /**
- * @description Gets the files needed for auditing, ignores files from .dddignore(s)
+ * @description Navigate through file pathes, auditing any file that is not in the .dddignore
  */
-function createAuditList() {
-  console.log('Creating audit list')
+function auditNavigator(root, dddignore) { // TODO A LOT MORE TESTING!!!
+  readdirSync(root).forEach(item => {
+    const FULL_PATH = path.join(root, item);
+    console.log(`Checking ${FULL_PATH}`)
 
-  // using .endsWith should work because for just file names those
-  // should be in the top level directory, anything in lower level directories should apply.
-  // might need to alter dddignore collection for array of objects as follows
-  // {
-  //  "highest-directory": src
-  //  "ignore": index.html (example)
-  // }
-  // The above could likely be used for a path .contains(src) and .endsWith(index.html)
+    if (item !== "node_modules" && item !== ".git" && item !== "dist" && statSync(FULL_PATH).isDirectory()) { // Directory Navigator
+      console.log("Checking directory against DDDIgnore")
+      if (dddignore.length !== 0) {
+        dddignore.forEach(ignore => {
+          if (!ignore.highest_path.startsWith(root) && ignore.name !== item && ignore.type !== "directory") {
+            auditNavigator(FULL_PATH, dddignore);
+          }
+        })
+      }
+      else {
+        auditNavigator(FULL_PATH, dddignore)
+      }
+    }
+    else { 
+      if (dddignore.length !== 0) {
+        dddignore.forEach(ignore => {
+          // Check the file against all extensions first, then run it against all type === "file". If neither apply, then audit file
+          if (!ignore.highest_path.startsWith(root) && !item.endsWith(ignore.name) && ignore.type !== "extension") {
+            if (ignore.name !== item && ignore.type !== "file") {
+              auditFile(FULL_PATH)
+            }
+          }
+        });
+      } else {
+        auditFile(FULL_PATH)
+      }
+    }
+  })
+}
+
+function auditFile(fileLocation) {
+  console.log(`Auditing ${fileLocation}`)
+  // let lines = readFileSync(fileLocation, 'utf-8').split('\n').filter(Boolean);
 }
